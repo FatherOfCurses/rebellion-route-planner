b5f47268153063fdd69292f0d02c5a944b662517
FatherOfCurses <colin.principe@gmail.com>
2024-12-13 14:08:35 -0500

    Add all repository files

--- src/main/java/org/rebelalliance/flightplanner/repositories/BookingRepository.java
+++ src/main/java/org/rebelalliance/flightplanner/repositories/BookingRepository.java
@@ -1,9 +1,9 @@
 package org.rebelalliance.flightplanner.repositories;
 
-import org.rebelalliance.flightplanner.model.TripEntity;
+import org.rebelalliance.flightplanner.model.BookingEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface TripRepository extends JpaRepository<TripEntity, UUID> {
+public interface BookingRepository extends JpaRepository<BookingEntity, UUID> {
 }
--- src/main/java/org/rebelalliance/flightplanner/repositories/CargoRepository.java
+++ src/main/java/org/rebelalliance/flightplanner/repositories/CargoRepository.java
@@ -1,9 +1,9 @@
 package org.rebelalliance.flightplanner.repositories;
 
-import org.rebelalliance.flightplanner.model.TripEntity;
+import org.rebelalliance.flightplanner.model.CargoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface TripRepository extends JpaRepository<TripEntity, UUID> {
+public interface CargoRepository extends JpaRepository<CargoEntity, UUID> {
 }
